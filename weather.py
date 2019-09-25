3a42c6a585ef10e7639ce471883ef3155c98444c
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-09-25 12:43:45 -0700

    completed function to get location_key

--- weather.py
+++ weather.py
@@ -1,8 +1,20 @@
+import os, requests
 
+api_key = os.environ['ACCUWEATHER_API_KEY']
 
 def get_location_key():
 	"""Takes zipcode, queries Accuweather API and returns location key"""
 
-	zipcode = 94607
+	zipcode = '94607'
 
-	
+	# url = f'http://dataservice.accuweather.com/locations/v1/search?apikey={api_key}&q={zipcode}'
+
+	# response = requests.get(url)
+
+	# data = response.json()
+
+	# location_key = data[0]['Key']
+
+	location_key = '39601_PC'
+
+	return location_key
