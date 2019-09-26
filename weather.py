9a93d3d18bae2a991b2f66599975cbcdd571510f
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-09-26 11:17:08 -0700

    added daily forecast and finalized layout

--- weather.py
+++ weather.py
@@ -20,20 +20,11 @@ def get_location_key():
 
 	else:
 
-		# url = f'http://dataservice.accuweather.com/locations/v1/search?apikey={api_key}&q={zipcode}'
+		url = f'http://dataservice.accuweather.com/locations/v1/search?apikey={api_key}&q={zipcode}'
 
-		# response = requests.get(url)
+		response = requests.get(url)
 
-		# data = response.json()
-
-		data = [{'Version': 1, 'Key': '5705_PC', 'Type': 'PostalCode', 'Rank': 45, 'LocalizedName': 'Rochester', 'EnglishName': 'Rochester', 'PrimaryPostalCode': '14623', 
-				'Region': {'ID': 'NAM', 'LocalizedName': 'North America', 'EnglishName': 'North America'}, 'Country': {'ID': 'US', 'LocalizedName': 'United States', 'EnglishName': 'United States'}, 
-				'AdministrativeArea': {'ID': 'NY', 'LocalizedName': 'New York', 'EnglishName': 'New York', 'Level': 1, 'LocalizedType': 'State', 'EnglishType': 'State', 'CountryID': 'US'}, 
-				'TimeZone': {'Code': 'EDT', 'Name': 'America/New_York', 'GmtOffset': -4.0, 'IsDaylightSaving': True, 'NextOffsetChange': '2019-11-03T06:00:00Z'}, 
-				'GeoPosition': {'Latitude': 43.088, 'Longitude': -77.644, 'Elevation': {'Metric': {'Value': 164.0, 'Unit': 'm', 'UnitType': 5}, 
-				'Imperial': {'Value': 537.0, 'Unit': 'ft', 'UnitType': 0}}}, 'IsAlias': False, 'ParentCity': {'Key': '329674', 'LocalizedName': 'Rochester', 'EnglishName': 'Rochester'}, 
-				'SupplementalAdminAreas': [{'Level': 2, 'LocalizedName': 'Monroe', 'EnglishName': 'Monroe'}], 'DataSets': ['Alerts', 'DailyAirQualityForecast', 'DailyPollenForecast', 'ForecastConfidence', 'MinuteCast', 'Radar']}]
-
+		data = response.json()
 
 		location_key = data[0]['Key']
 
@@ -45,19 +36,14 @@ def get_location_key():
 def get_hourly_forecast(location_key, city):
 	"""Gets next 12 hours of hourly forecast and prints to terminal"""
 
-	# url = f'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/{location_key}?apikey={api_key}'
+	url = f'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/{location_key}?apikey={api_key}'
 
-	# response = requests.get(url)
+	response = requests.get(url)
 
-	# data = response.json()
-
-	data = [{'DateTime': '2019-09-25T20:00:00-04:00', 'EpochDateTime': 1569456000, 'WeatherIcon': 35, 'IconPhrase': 'Partly cloudy', 'HasPrecipitation': False, 'IsDaylight': False, 
-			'Temperature': {'Value': 72.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 0, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=1&hbhhour=20&lang=en-us', 
-			'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=1&hbhhour=20&lang=en-us'}, 
-			{'DateTime': '2019-09-25T21:00:00-04:00', 'EpochDateTime': 1569459600, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 69.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 0, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=1&hbhhour=21&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=1&hbhhour=21&lang=en-us'}, {'DateTime': '2019-09-25T22:00:00-04:00', 'EpochDateTime': 1569463200, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 67.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 0, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=1&hbhhour=22&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=1&hbhhour=22&lang=en-us'}, {'DateTime': '2019-09-25T23:00:00-04:00', 'EpochDateTime': 1569466800, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 66.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 0, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=1&hbhhour=23&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=1&hbhhour=23&lang=en-us'}, {'DateTime': '2019-09-26T00:00:00-04:00', 'EpochDateTime': 1569470400, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 65.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 0, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=0&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=0&lang=en-us'}, {'DateTime': '2019-09-26T01:00:00-04:00', 'EpochDateTime': 1569474000, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 64.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 2, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=1&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=1&lang=en-us'}, {'DateTime': '2019-09-26T02:00:00-04:00', 'EpochDateTime': 1569477600, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 64.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 7, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=2&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=2&lang=en-us'}, {'DateTime': '2019-09-26T03:00:00-04:00', 'EpochDateTime': 1569481200, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 63.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 7, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=3&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=3&lang=en-us'}, {'DateTime': '2019-09-26T04:00:00-04:00', 'EpochDateTime': 1569484800, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 63.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 6, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=4&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=4&lang=en-us'}, {'DateTime': '2019-09-26T05:00:00-04:00', 'EpochDateTime': 1569488400, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 62.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 5, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=5&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=5&lang=en-us'}, {'DateTime': '2019-09-26T06:00:00-04:00', 'EpochDateTime': 1569492000, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 62.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 5, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=6&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=6&lang=en-us'}, {'DateTime': '2019-09-26T07:00:00-04:00', 'EpochDateTime': 1569495600, 'WeatherIcon': 36, 'IconPhrase': 'Intermittent clouds', 'HasPrecipitation': False, 'IsDaylight': False, 'Temperature': {'Value': 61.0, 'Unit': 'F', 'UnitType': 18}, 'PrecipitationProbability': 8, 'MobileLink': 'http://m.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=7&lang=en-us', 'Link': 'http://www.accuweather.com/en/us/pittsburgh-pa/15219/hourly-weather-forecast/6002_pc?day=2&hbhhour=7&lang=en-us'}]
+	data = response.json()
 
 	print(f'\nWEATHER FORECAST FOR: {city}\n')
-	print('Hourly Forecast:')
+	print('Today\'s Hourly Forecast:')
 
 	for item in data:
 		short_date = item['DateTime'][:-6]
@@ -67,11 +53,39 @@ def get_hourly_forecast(location_key, city):
 
 		temperature = str(int(item['Temperature']['Value'])) + item['Temperature']['Unit']
 
-		precipitation = str(item['PrecipitationProbability'] * 10) + '%'
+		precipitation = str(item['PrecipitationProbability']) + '%'
 
 		condition = item['IconPhrase']
 
 		print(f'\t{time}:\t{temperature}\tPrecip:\t{precipitation}\tConditions: {condition}')
+
+
+def get_daily_forecast(location_key):
+	"""Gets 5 day forecast and prints to terminal"""
+
+	url = f'http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}?apikey={api_key}&details=true'
+
+	response = requests.get(url)
+
+	data = response.json()
+
+	print('\nDaily Forecast:')
+
+	for forecast in data['DailyForecasts']:
+
+		short_date = forecast['Date'][:10]
+		datetime_object = datetime.strptime(short_date, '%Y-%m-%d')
+		date = datetime_object.strftime('%a, %b %d')
+
+		low_temp = str(int(forecast['Temperature']['Minimum']['Value'])) + str(forecast['Temperature']['Minimum']['Unit'])
+		high_temp = str((int(forecast['Temperature']['Maximum']['Value']))) + str(forecast['Temperature']['Maximum']['Unit'])
+
+		precipitation = str(forecast['Day']['PrecipitationProbability']) + '%'
+
+		condition = forecast['Day']['IconPhrase']
+
+
+		print(f'\t{date}: \tLow: {low_temp}\tHigh: {high_temp}\tPrecip: {precipitation}\tConditions: {condition}')
 
 
 def display_forecast():
@@ -84,6 +98,10 @@ def display_forecast():
 		location_key, city = result
 
 		get_hourly_forecast(location_key, city)
+		get_daily_forecast(location_key)
 
+		print('\nData courtesy of AccuWeather')
 
 display_forecast()
+
+
