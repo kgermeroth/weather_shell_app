03508781791db1e1981efe14b58166eced0fb793
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-10-02 09:46:10 -0700

    updated 'install.sh' to take all commands in one line and set up the sym link

--- install.sh
+++ install.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 
-echo Enter your AccuWeather API Key:
-read apiKey
-echo Enter default zipcode:
-read zipcode
+set -e
+
+if [ $# -ne 3 ]; then
+	echo invalid usage:
+	echo ./install.sh DIRECTORY_PATH API_KEY ZIPCODE 
+	exit
+fi
+
+directory="$1"
+api_key="$2"
+zipcode="$3"
+
+install_script=$(realpath $0)
+install_directory=$(dirname $install_script)
+
+weather_path=${install_directory}/weather.py
+
+cd ${directory}
+
+ln -s ${weather_path} weather
 
 mkdir -p ~/.config/weather_app
 touch ~/.config/weather_app/config.json
 
 cat << EOF > ~/.config/weather_app/config.json
 {
-	"ACCUWEATHER_API_KEY" : "${apiKey}",
+	"ACCUWEATHER_API_KEY" : "${api_key}",
 	"DEFAULT_ZIPCODE" : "${zipcode}"
 }
 EOF
 
-pip3 install -r requirements.txt
+pip3 install -r ${install_directory}/requirements.txt
 
